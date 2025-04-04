2deb2359197ef1c76c9a8e19a41d14337d8029ca
Jessica Pergentino <jessica.pergentino@capgemini.com>
2025-04-04 15:02:30 -0300

    Projeto Code Swag Finalizado

---
+++ coder-swag/Model/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Capgemini on 04/04/25.
+//
+
+import Foundation
+struct Product {
+    private(set) public var title: String
+    private(set) public var price: String
+    private(set) public var imageName: String
+    
+    init(title: String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+}
